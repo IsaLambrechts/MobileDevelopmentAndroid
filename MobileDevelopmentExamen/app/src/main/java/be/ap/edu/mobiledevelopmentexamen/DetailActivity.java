fc2bc363a1dc7f2a2e0bad3771640f6e4ae60694
IsaLambrechts <isa.lambrechts@hotmail.be>
2017-10-06 15:11:08 +0200

    Add data to db

--- MobileDevelopmentExamen/app/src/main/java/be/ap/edu/mobiledevelopmentexamen/DetailActivity.java
+++ MobileDevelopmentExamen/app/src/main/java/be/ap/edu/mobiledevelopmentexamen/DetailActivity.java
@@ -2,9 +2,12 @@ package be.ap.edu.mobiledevelopmentexamen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.io.Serializable;
 
@@ -12,9 +15,11 @@ public class DetailActivity extends AppCompatActivity {
 
     private TextView textView;
     private TextView textView1;
+    private TextView omschrijving_text;
     private Button button;
     private Serializable text;
     private Serializable text1;
+    private MapSQLiteHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +35,22 @@ public class DetailActivity extends AppCompatActivity {
         textView1.setText(text1.toString());
 
         button = (Button) findViewById(R.id.button);
+
+        helper = new MapSQLiteHelper(this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                omschrijving_text = (TextView) findViewById(R.id.omschrijving_text);
+                String omschrijving = omschrijving_text.getText().toString();
+                Log.d("SQLITE", omschrijving);
+                if(!(omschrijving.matches(""))) {
+                    helper.addContact(Double.parseDouble(text.toString()), Double.parseDouble(text1.toString()), omschrijving);
+                    System.out.println(helper.getTableAsString());
+                }
+            }
+        });
 
     }
 }
--- MobileDevelopmentExamen/app/src/main/java/be/ap/edu/mobiledevelopmentexamen/MapSQLiteHelper.java
+++ MobileDevelopmentExamen/app/src/main/java/be/ap/edu/mobiledevelopmentexamen/MapSQLiteHelper.java
@@ -1,8 +1,74 @@
 package be.ap.edu.mobiledevelopmentexamen;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
 /**
  * Created by CaruCath on 06-10-17.
  */
 
-class MapSQLiteHelper {
+class MapSQLiteHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "mapsaver.db";
+    private static final String TABLE_NAME = "coordinates";
+    private static final int DATABASE_VERSION = 2;
+
+    public MapSQLiteHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        String CREATE_USERS_TABLE = "CREATE TABLE " + TABLE_NAME + "(_id INTEGER PRIMARY KEY AUTOINCREMENT, longitude REAL, latitude REAL, beschrijving STRING)";
+        db.execSQL(CREATE_USERS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public void addContact(double lon, double lat, String beschrijving) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        //db.delete(TABLE_CONTACTS, null, null);
+        //db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+
+        ContentValues values = new ContentValues();
+        values.put("longitude", lon);
+        values.put("latitude", lat);
+        values.put("beschrijving", "test");
+
+        db.insert(TABLE_NAME, null, values);
+        db.close();
+    }
+
+    public String getTableAsString() {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Log.d("SQLITE", "getTableAsString called");
+        String tableString = String.format("Table %s:\n", TABLE_NAME);
+        Cursor allRows  = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        System.out.println(allRows.getCount());
+        if (allRows.moveToFirst() ){
+            String[] columnNames = allRows.getColumnNames();
+            do {
+                for (String name: columnNames) {
+                    tableString += String.format("%s: %s\n", name,
+                            allRows.getString(allRows.getColumnIndex(name)));
+                }
+                tableString += "\n";
+
+            } while (allRows.moveToNext());
+        }
+
+        allRows.close();
+
+        return tableString;
+    }
 }
